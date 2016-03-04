c63a6d34339366a30ca7b693e29c7bf9a4e05c7a
Marissa96 <marissa.boswall96@gmail.com>
2016-03-03 19:45:20 -0500

    Wasn't working; re-did the whole main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-var popup = alert('Let’s make a story together!');
+alert('Let’s make a story together!');
+
 var colour = prompt('A colour (orange, indigo, etc.)');
 var creature = prompt('A creature (dragon, moth, etc.)');
-var adjective = prompt('An adjective (beautiful, super, etc.)');
-var pastVerb = prompt('A past tense verb (ran, burped, etc.)');
-var story = prompt('Which story would you like? \na) Snacking\nb) Escaping\nc) Winning');
+var adjective  = prompt('An adjective (beautiful, super, etc.)');
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+var story = prompt('Which story would you like?\n a) Snacking\n b) Escaping\n c) Winning');
+
+if (story == 'a') {
+  document.write('After snacking on '  + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verb + ' for hours.');
+}
+
+if (story == 'b') {
+  document.write('Ameilia ' + verb + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
+}
+
+if (story == 'c') {
+  document.write('Jackson chose his ' + adjective + ',' + colour + ' ' + creature + ' card and ' + verb + ' it to the table knowing he won.');
+}
