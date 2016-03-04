601cc7cfc0cd59141d4ca36a7e428b34908a5af0
Marissa96 <marissa.boswall96@gmail.com>
2016-03-03 19:47:10 -0500

    Fixed all problems

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-alert('Let’s make a story together!');
-
 var colour = prompt('A colour (orange, indigo, etc.)');
 var creature = prompt('A creature (dragon, moth, etc.)');
 var adjective  = prompt('An adjective (beautiful, super, etc.)');
 var verb = prompt('A past tense verb (ran, burped, etc.)');
 var story = prompt('Which story would you like?\n a) Snacking\n b) Escaping\n c) Winning');
+
+alert('Let’s make a story together!');
 
 if (story == 'a') {
   document.write('After snacking on '  + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verb + ' for hours.');
